ecf6f3dc34d461ffffaa657e5189a7bafe8725cd
grantbirki <grantbirki@github.com>
2022-09-12 22:38:01 -0600

    src test and cov folder structure

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import * as core from '@actions/core'
+// import {reactEmote} from './functions/react-emote'
+// import * as github from '@actions/github'
+// import {context} from '@actions/github'
+// import dedent from 'dedent-js'
+
+export async function run() {
+}
