b216d969a0bb22a1e539b10b0f16266f2484f96b
grantbirki <grantbirki@github.com>
2022-09-16 11:49:50 -0600

    git diff files

--- src/functions/diff.js
+++ src/functions/diff.js
@@ -1,32 +1,36 @@
 import * as core from '@actions/core'
-import parseGitDiff from 'parse-git-diff';
-import { exec } from 'child_process';
+import parseGitDiff from 'parse-git-diff'
+import {exec} from 'child_process'
 
 // Helper function to get the diff from the git command
 // :returns: The diff object which is parsed git diff
 // If an error occurs, setFailed is called and it returns null
 export function gitDiff() {
-    try {
-        // Get the base branch to use for the diff
-        const baseBranch = core.getInput('base_branch');
+  try {
+    // Get the base branch to use for the diff
+    const baseBranch = core.getInput('base_branch')
 
-        exec(`git diff ${baseBranch}`, (error, stdout, stderr) => {
-            if (error) {
-                core.setFailed(
-                    `git diff error: ${error.message}`
-                )
-                return;
-            }
-            if (stderr) {
-                core.setFailed(`git diff error: ${stderr}`);
-                return;
-            }
+    exec(`git diff ${baseBranch}`, (error, stdout, stderr) => {
+      if (error) {
+        core.setFailed(`git diff error: ${error.message}`)
+        return
+      }
+      if (stderr) {
+        core.setFailed(`git diff error: ${stderr}`)
+        return
+      }
 
-            const diff = parseGitDiff(stdout);
-            core.debug(JSON.stringify(diff));
-            return diff;
-        });
-    } catch (e) {
-        core.setFailed(`Error getting git diff: ${e}`);
-    }
+      // Raw diff
+      core.debug(`raw git diff: ${stdout}`)
+      core.setOutput('raw-diff', stdout)
+
+      // JSON diff
+      const diff = parseGitDiff(stdout)
+      core.debug(JSON.stringify(diff))
+      core.setOutput('diff', JSON.stringify(diff))
+      return diff
+    })
+  } catch (e) {
+    core.setFailed(`Error getting git diff: ${e}`)
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
-// import * as core from '@actions/core'
-// import {reactEmote} from './functions/react-emote'
-// import * as github from '@actions/github'
-// import {context} from '@actions/github'
-// import dedent from 'dedent-js'
+import {gitDiff} from './functions/git-diff'
 
 export async function run() {
-  console.log('Hello, world!!')
-  console.log('todo')
+  gitDiff()
 }
 
 run()
