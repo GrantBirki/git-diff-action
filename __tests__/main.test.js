a67794f5476739dce140463a7715cf50841c1b35
Grant Birkinbine <grantbirki@github.com>
2022-09-24 13:10:08 -0600

    fmt

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -2,40 +2,40 @@ import {run} from '../src/main'
 import * as gitDiff from '../src/functions/git-diff'
 
 const SAMPLE_DIFF = {
-    "type": "GitDiff",
-    "files": [
+  type: 'GitDiff',
+  files: [
+    {
+      type: 'AddedFile',
+      chunks: [
         {
-            "type": "AddedFile",
-            "chunks": [
-                {
-                    "type": "Chunk",
-                    "toFileRange": {
-                        "start": 1,
-                        "lines": 47
-                    },
-                    "fromFileRange": {
-                        "start": 0,
-                        "lines": 0
-                    },
-                    "changes": [
-                        {
-                            "type": "AddedLine",
-                            "lineAfter": 1,
-                            "content": "name: package-check"
-                        }
-                    ]
-                }
-            ],
-            "path": ".github/workflows/package-check.yml"
+          type: 'Chunk',
+          toFileRange: {
+            start: 1,
+            lines: 47
+          },
+          fromFileRange: {
+            start: 0,
+            lines: 0
+          },
+          changes: [
+            {
+              type: 'AddedLine',
+              lineAfter: 1,
+              content: 'name: package-check'
+            }
+          ]
         }
-    ]
+      ],
+      path: '.github/workflows/package-check.yml'
+    }
+  ]
 }
 
 beforeEach(() => {
-    jest.spyOn(gitDiff, 'gitDiff').mockImplementation(() => {
-      return SAMPLE_DIFF
-    })
+  jest.spyOn(gitDiff, 'gitDiff').mockImplementation(() => {
+    return SAMPLE_DIFF
   })
+})
 
 test('executes main', async () => {
   expect(await run()).toStrictEqual(undefined)
