e3abfde88f7af96733fe19e4956de30e019b5100
LukeP Labrys <luke.p@labrys.tech>
2023-07-19 13:05:51 +0100

    Run npm build

--- src/functions/git-diff.js
+++ src/functions/git-diff.js
@@ -16,45 +16,49 @@ export function gitDiff() {
     const maxBufferSize = core.getInput('max_buffer_size')
     core.debug(`max_buffer_size: ${maxBufferSize}`)
 
-    exec(`git diff ${baseBranch} ${searchPath}`, {maxBuffer: maxBufferSize}, (error, stdout, stderr) => {
-      if (error) {
-        core.setFailed(`git diff error: ${error.message}`)
-        return
+    exec(
+      `git diff ${baseBranch} ${searchPath}`,
+      {maxBuffer: maxBufferSize},
+      (error, stdout, stderr) => {
+        if (error) {
+          core.setFailed(`git diff error: ${error.message}`)
+          return
+        }
+        if (stderr) {
+          core.setFailed(`git diff error: ${stderr}`)
+          return
+        }
+
+        // Raw diff
+        core.debug(`raw git diff: ${stdout}`)
+        core.setOutput('raw-diff', stdout)
+
+        // Write the raw diff to a file if the path is provided
+        const rawPath = core.getInput('raw_diff_file_output')
+        if (rawPath) {
+          core.debug(`writing raw diff to ${rawPath}`)
+          core.setOutput('raw-diff-path', rawPath)
+          writeFileSync(rawPath, stdout)
+        }
+
+        // JSON diff
+        const diff = parseGitDiff(stdout)
+        core.debug(JSON.stringify(diff))
+
+        const jsonDiff = JSON.stringify(diff)
+        core.setOutput('json-diff', jsonDiff)
+
+        // Write the JSON diff to a file if the path is provided
+        const jsonPath = core.getInput('json_diff_file_output')
+        if (jsonPath) {
+          core.debug(`writing json diff to ${jsonPath}`)
+          core.setOutput('json-diff-path', jsonPath)
+          writeFileSync(jsonPath, jsonDiff)
+        }
+
+        return diff
       }
-      if (stderr) {
-        core.setFailed(`git diff error: ${stderr}`)
-        return
-      }
-
-      // Raw diff
-      core.debug(`raw git diff: ${stdout}`)
-      core.setOutput('raw-diff', stdout)
-
-      // Write the raw diff to a file if the path is provided
-      const rawPath = core.getInput('raw_diff_file_output')
-      if (rawPath) {
-        core.debug(`writing raw diff to ${rawPath}`)
-        core.setOutput('raw-diff-path', rawPath)
-        writeFileSync(rawPath, stdout)
-      }
-
-      // JSON diff
-      const diff = parseGitDiff(stdout)
-      core.debug(JSON.stringify(diff))
-
-      const jsonDiff = JSON.stringify(diff)
-      core.setOutput('json-diff', jsonDiff)
-
-      // Write the JSON diff to a file if the path is provided
-      const jsonPath = core.getInput('json_diff_file_output')
-      if (jsonPath) {
-        core.debug(`writing json diff to ${jsonPath}`)
-        core.setOutput('json-diff-path', jsonPath)
-        writeFileSync(jsonPath, jsonDiff)
-      }
-
-      return diff
-    })
+    )
   } catch (e) {
     core.setFailed(`error getting git diff: ${e}`)
   }
