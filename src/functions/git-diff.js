bce88b467c94db5afeafca99f1b039ad3b6dcf8e
GrantBirki <grantbirki@github.com>
2024-03-18 22:22:35 -0600

    `promisify` exec

--- src/functions/git-diff.js
+++ src/functions/git-diff.js
@@ -1,13 +1,16 @@
 import * as core from '@actions/core'
 import parseGitDiff from 'parse-git-diff'
-import {exec} from 'child_process'
-import {writeFileSync} from 'fs'
+import { exec } from 'child_process'
+import { promisify } from 'util';
+import { writeFileSync } from 'fs'
 
 // Helper function to get the diff from the git command
 // :returns: The diff object which is parsed git diff
 // If an error occurs, setFailed is called and it returns null
-export function gitDiff() {
+export async function gitDiff() {
   try {
+    const execAsync = promisify(exec)
+
     // Get the base branch to use for the diff
     const baseBranch = core.getInput('base_branch')
     core.debug(`base_branch: ${baseBranch}`)
@@ -28,57 +31,49 @@ export function gitDiff() {
       maxBufferSize = 1000000
     }
 
-    exec(
-      `git diff ${baseBranch} ${searchPath}`,
-      {maxBuffer: maxBufferSize},
-      (error, stdout, stderr) => {
-        if (error) {
-          core.setFailed(`git diff error: ${error.message}`)
-          return
-        }
-        if (stderr) {
-          core.setFailed(`git diff error: ${stderr}`)
-          return
-        }
+    const { stdout, stderr } = await execAsync(`git diff ${baseBranch} ${searchPath}`, { maxBuffer: maxBufferSize });
 
-        // only log the raw diff if the Action is explicitly set to run in debug mode
-        core.debug(`raw git diff: ${stdout}`)
-        if (fileOutputOnly === false) {
-          // only set the output if fileOutputOnly is false
-          core.setOutput('raw-diff', stdout)
-        }
+    if (stderr) {
+      core.setFailed(`git diff error: ${stderr}`)
+      return
+    }
 
-        // Write the raw diff to a file if the path is provided
-        const rawPath = core.getInput('raw_diff_file_output')
-        if (rawPath) {
-          core.debug(`writing raw diff to ${rawPath}`)
-          core.setOutput('raw-diff-path', rawPath)
-          writeFileSync(rawPath, stdout)
-        }
+    // only log the raw diff if the Action is explicitly set to run in debug mode
+    core.debug(`raw git diff: ${stdout}`)
+    if (fileOutputOnly === false) {
+      // only set the output if fileOutputOnly is false
+      core.setOutput('raw-diff', stdout)
+    }
 
-        // JSON diff
-        const diff = parseGitDiff(stdout)
-        const jsonDiff = JSON.stringify(diff)
+    // Write the raw diff to a file if the path is provided
+    const rawPath = core.getInput('raw_diff_file_output')
+    if (rawPath) {
+      core.debug(`writing raw diff to ${rawPath}`)
+      core.setOutput('raw-diff-path', rawPath)
+      writeFileSync(rawPath, stdout)
+    }
 
-        // only log the json diff if the Action is explicitly set to run in debug mode
-        core.debug(JSON.stringify(diff))
+    // JSON diff
+    const diff = parseGitDiff(stdout)
+    const jsonDiff = JSON.stringify(diff)
 
-        // only set the output if fileOutputOnly is false
-        if (fileOutputOnly === false) {
-          core.setOutput('json-diff', jsonDiff)
-        }
+    // only log the json diff if the Action is explicitly set to run in debug mode
+    core.debug(`jsonDiff: ${jsonDiff}`)
 
-        // Write the JSON diff to a file if the path is provided
-        const jsonPath = core.getInput('json_diff_file_output')
-        if (jsonPath) {
-          core.debug(`writing json diff to ${jsonPath}`)
-          core.setOutput('json-diff-path', jsonPath)
-          writeFileSync(jsonPath, jsonDiff)
-        }
+    // only set the output if fileOutputOnly is false
+    if (fileOutputOnly === false) {
+      core.setOutput('json-diff', jsonDiff)
+    }
 
-        return diff
-      }
-    )
+    // Write the JSON diff to a file if the path is provided
+    const jsonPath = core.getInput('json_diff_file_output')
+    if (jsonPath) {
+      core.debug(`writing json diff to ${jsonPath}`)
+      core.setOutput('json-diff-path', jsonPath)
+      writeFileSync(jsonPath, jsonDiff)
+    }
+
+    return diff
   } catch (e) {
     core.setFailed(`error getting git diff: ${e}`)
   }
